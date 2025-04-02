558246b764aed2ff3396d56d05992b77c794cd84
Florence LECLERCQ <f.leclercq@helmo.be>
2025-04-02 16:22:02 +0200

    ajustement session

--- php/session.inc.php
+++ php/session.inc.php
@@ -2,4 +2,5 @@
 //si l'utilisateur n'est pas enregistré dans le tableau des sessions, on redirige vers la page de connexion
 if (!isset($_SESSION['nom_user'])) {
     header("location:" . BASE_URL . "/public/connect.php");
+    exit();
 }
