5b99d090aa82c0e7297a52e8f723aab480abb0aa
Florence LECLERCQ <f.leclercq@helmo.be>
2025-04-02 14:49:45 +0200

    suppression reférence user (sql) + ajustement session

--- php/session.inc.php
+++ php/session.inc.php
@@ -1,10 +1,5 @@
 <?php
 
-if (isset($_SESSION['nom_user'])) {
-    $nom_session = $_SESSION['nom_user'];
-    $id_user_session = $_SESSION['id_user'];
-} else {
-    $path = $_SERVER['PHP_SELF'];
-    $file = basename($path);
-    if ($file !== 'connect.php') header("location:" . BASE_URL . "/public/connect.php");
+if (!isset($_SESSION['nom_user'])) {
+    header("location:" . BASE_URL . "/public/connect.php");
 }
